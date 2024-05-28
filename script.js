091f2bb8a4918daf57ece22beea18ea6cb3c6e9d
janzahradka <jan.zahradka@gmail.com>
2024-05-28 23:04:51 +0200

    adding slider position retain

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const baseUrls = {
-        'oblacnost': 'https://server1.meteopress.cz/icond2/oblc',
-        'pocasi': 'https://server1.meteopress.cz/icond2/pocasi',
-        'kumuly': 'https://server1.meteopress.cz/icond2/bascu'
-    };
+    const product = {
+        'oblc': 'oblc',
+        'pocasi': 'pocasi',
+        'bascu': 'bascu',
+        'cu': 'cu',
+        'tmp2m': 'tmp2m',
+        'tlcu': 'tlcu',
+        'vlhk700': 'vlhk700',
+        'vitr10m': 'vitr10m',
+        'narazy': 'narazy'
+    }
+    const serverUrl = 'https://server1.meteopress.cz/icond2/';
     const extension = '.png';
     const numberOfImages = 48;
     const imageCache = {};
     const imageDisplay = document.getElementById('image-display');
     const slider = document.getElementById('slider');
+    let currentImageIndex = 1; // Store the current slider position
 
     function generateImageUrls(setType) {
         const urls = [];
         for (let i = 1; i <= numberOfImages; i++) {
-            urls.push(`${baseUrls[setType]}${i}${extension}`);
+            urls.push(`${serverUrl}${product[setType]}${i}${extension}`);
         }
         return urls;
     }
 
-    function preloadImages(imageUrls) {
+    function preloadImages(imageUrls, initialImageIndex) {
         imageUrls.forEach((url, index) => {
             const img = new Image();
             img.src = url;
             img.onload = () => {
                 imageCache[index + 1] = img.src;
-                if (index === 0) {
-                    // Set the first image as the initial source
+                if (index + 1 === initialImageIndex) {
+                    // Set the initial image based on the slider position
                     imageDisplay.src = img.src;
                 }
             };
@@ -35,15 +43,15 @@ document.addEventListener('DOMContentLoaded', function () {
     window.loadImages = function(setType) {
         const imageUrls = generateImageUrls(setType);
         slider.max = numberOfImages; // Set the max value of the slider dynamically
-        preloadImages(imageUrls);
+        preloadImages(imageUrls, currentImageIndex); // Pass the current image index
     }
 
     // Load the default set of images (Oblacnost)
-    loadImages('oblacnost');
+    loadImages('oblc');
 
     // Update the image based on the slider value
     slider.addEventListener('input', (event) => {
-        const imageIndex = event.target.value;
-        imageDisplay.src = imageCache[imageIndex];
+        currentImageIndex = parseInt(event.target.value, 10); // Update the current image index
+        imageDisplay.src = imageCache[currentImageIndex];
     });
 });
