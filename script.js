438b927373b73949d3717893c69e820dbf354cea
janzahradka <jan.zahradka@gmail.com>
2024-05-31 18:34:26 +0200

    Refactoring of product structure

--- script.js
+++ script.js
@@ -1,32 +1,123 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const product = {
-        'oblc': 'oblc',
-        'pocasi': 'pocasi',
-        'bascu': 'bascu',
-        'cu': 'cu',
-        'tmp2m': 'tmp2m',
-        'tlcu': 'tlcu',
-        'vlhk700': 'vlhk700',
-        'vitr10m': 'vitr10m',
-        'narazy': 'narazy',
-        'vi950': 'vi950',
-        'vi850': 'vi850',
-        'vi700': 'vi700',
-        'vi600': 'vi600',
-        'vi500': 'vi500'
-    }
-    const serverUrl = 'https://server1.meteopress.cz/icond2/';
-    const extension = '.png';
-    const numberOfImages = 48;
+    const groups = {
+        'icon-d2': {
+            label: 'ICON D2',
+            serverUrl: 'https://server1.meteopress.cz/icond2/',
+            product: {
+                'oblc': {
+                    label: 'Oblačnost',
+                    step: 1,
+                    extension: '.png',
+                    numberOfImages: 48
+                },
+                'pocasi': {
+                    label: 'Srážky',
+                    step: 1,
+                    extension: '.png',
+                    numberOfImages: 48
+                },
+                'cu': {
+                    label: 'Čistá + Cu',
+                    step: 1,
+                    extension: '.png',
+                    numberOfImages: 48
+                },
+                'bascu': {
+                    label: 'Kumuly',
+                    step: 1,
+                    extension: '.png',
+                    numberOfImages: 48
+                },
+                'tlcu': {
+                    label: 'Toušťka Cu',
+                    step: 1,
+                    extension: '.png',
+                    numberOfImages: 48
+                },
+                'tmp2m': {
+                    label: 'Teplota 2m',
+                    step: 1,
+                    extension: '.png',
+                    numberOfImages: 48
+                },
+                'vlhk700': {
+                    label: 'Vlhkost 3km',
+                    step: 1,
+                    extension: '.png',
+                    numberOfImages: 48
+                },
+                'vitr10m': {
+                    label: 'Vítr 10m',
+                    step: 1,
+                    extension: '.png',
+                    numberOfImages: 48
+                },
+                'narazy': {
+                    label: 'Nárazy',
+                    step: 1,
+                    extension: '.png',
+                    numberOfImages: 48
+                },
+                'vi950': {
+                    label: 'Vítr 0,5km',
+                    step: 1,
+                    extension: '.png',
+                    numberOfImages: 48
+                },
+                'vi850': {
+                    label: 'Vítr 1,5km',
+                    step: 1,
+                    extension: '.png',
+                    numberOfImages: 48
+                },
+                'vi700': {
+                    label: 'Vítr 3km',
+                    step: 1,
+                    extension: '.png',
+                    numberOfImages: 48
+                },
+                'vi600': {
+                    label: 'Vítr 4km',
+                    step: 1,
+                    extension: '.png',
+                    numberOfImages: 48
+                },
+                'vi500': {
+                    label: 'Vítr 5,5km',
+                    step: 1,
+                    extension: '.png',
+                    numberOfImages: 48
+                }
+            }
+        },
+        'wetterzentrale': {
+            label: 'Wetterzentrale',
+            serverUrl: 'https://www.wetterzentrale.de/maps/',
+            product: {
+                'GFSOPEU06_': {
+                    label: 'T850hpa',
+                    step: 3,
+                    extension: '_2.png',
+                    numberOfImages: 129
+                },
+
+            }
+        }
+    };
+
+    let currentGroup = 'icon-d2';
     const imageCache = {};
     const imageDisplay = document.getElementById('image-display');
     const slider = document.getElementById('slider');
     let currentImageIndex = 1; // Store the current slider position
 
-    function generateImageUrls(setType) {
+    function generateImageUrls(group, setType) {
         const urls = [];
-        for (let i = 1; i <= numberOfImages; i++) {
-            urls.push(`${serverUrl}${product[setType]}${i}${extension}`);
+        const { serverUrl, product } = groups[group];
+        const { step, extension, numberOfImages } = product[setType];
+        for (let i = 0; i < numberOfImages; i++) {
+            const imageIndex = i * step;
+            urls.push(`${serverUrl}${setType}${imageIndex}${extension}`);
         }
         return urls;
     }
@@ -45,9 +136,28 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
+    function createDropdownMenu() {
+        const dropdownContent = document.getElementById('dropdown-content');
+        dropdownContent.innerHTML = ''; // Clear existing menu items
+
+        const product = groups[currentGroup].product;
+        for (const key in product) {
+            if (product.hasOwnProperty(key)) {
+                const menuItem = document.createElement('a');
+                menuItem.href = '#';
+                menuItem.textContent = product[key].label;
+                menuItem.onclick = function () {
+                    loadImages(key);
+                    toggleDropdown();
+                };
+                dropdownContent.appendChild(menuItem);
+            }
+        }
+    }
+
     window.loadImages = function(setType) {
-        const imageUrls = generateImageUrls(setType);
-        slider.max = numberOfImages; // Set the max value of the slider dynamically
+        const imageUrls = generateImageUrls(currentGroup, setType);
+        slider.max = imageUrls.length; // Set the max value of the slider dynamically
         preloadImages(imageUrls, currentImageIndex); // Pass the current image index
 
         // Hide the dropdown if the window width is less than 1340 pixels
@@ -56,9 +166,16 @@ document.addEventListener('DOMContentLoaded', function () {
             const dropdownContent = document.getElementById('dropdown-content');
             dropdownContent.style.display = 'none';
         }
-    }
+    };
 
-    // Load the default set of images (Oblacnost)
+    window.setGroup = function(group) {
+        currentGroup = group;
+        createDropdownMenu(); // Create menu items dynamically for the selected group
+        loadImages(Object.keys(groups[group].product)[0]); // Load the first set of images for the selected group
+    };
+
+    // Load the default set of images (ICON D2)
+    createDropdownMenu();
     loadImages('oblc');
 
     // Update the image based on the slider value
@@ -81,13 +198,26 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     };
 
+    window.toggleGroupDropdown = function() {
+        const dropdownContent = document.getElementById('group-dropdown-content');
+        if (dropdownContent.style.display === 'block') {
+            dropdownContent.style.display = 'none';
+        } else {
+            dropdownContent.style.display = 'block';
+        }
+    };
+
     // Close the dropdown if the user clicks outside of it
     window.onclick = function(event) {
         if (!event.target.matches('.dropbtn')) {
             const dropdownContent = document.getElementById('dropdown-content');
+            const groupDropdownContent = document.getElementById('group-dropdown-content');
             const windowWidth = window.innerWidth;
             if (dropdownContent.style.display === 'block' && windowWidth < 1340) {
                 dropdownContent.style.display = 'none';
+            }
+            if (groupDropdownContent.style.display === 'block') {
+                groupDropdownContent.style.display = 'none';
             }
         }
     };
